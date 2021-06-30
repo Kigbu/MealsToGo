f5dd3d82bfde514012e3d43bd1ac68fd25371d54
Kigbu <kigbua@gmail.com>
2021-06-30 09:20:06 +0100

    update - morning 30 june

---
+++ src/features/restaurants/components/restaurants-info.component.js
@@ -0,0 +1,7 @@
+import { React } from "react";
+import { Searchbar } from "react-native-paper";
+import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
+
+export const Resaturant = () => (
+    <Text>RestaurantsInfo</Text>
+)
---
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -0,0 +1,30 @@
+import { React } from "react";
+import { Searchbar } from "react-native-paper";
+import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
+
+export const Restaurants = () => (
+  <SafeAreaView style={styles.container}>
+    <View style={styles.search}>
+      <Searchbar />
+    </View>
+    <View style={styles.list}>
+      <RestaurantInfo
+    </View>
+  </SafeAreaView>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 16,
+    backgroundColor: "green",
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "blue",
+  },
+});
